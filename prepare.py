5c77f09f674c6efc6d46b167fa3d11d609f6cedd
adam-talbot <adam.talbot527@gmail.com>
2021-08-16 09:39:46 -0500

    made some modifications to prepare exercises after class walkthough

--- prepare.py
+++ prepare.py
@@ -3,6 +3,6 @@ import pandas as pd
 def prep_iris(df): # I will pass the unpreped df into the function
     df.drop(columns = ['species_id'], inplace = True)
     df = df.rename(columns = {'species_name' : 'species'})
-    df_dummy = pd.get_dummies(df['species'], drop_first=True)
+    df_dummy = pd.get_dummies(df['species'], drop_first=False)
     df = pd.concat([df, df_dummy], axis = 1)
     return df
